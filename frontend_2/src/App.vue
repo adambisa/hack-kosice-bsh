57a29d6b302236880efce2a8608a244034a87b1f
Filip Šipoš <filipsipo@gmail.com>
2023-04-01 17:17:51 +0200

    card

--- frontend_2/src/App.vue
+++ frontend_2/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-    <v-app>
-      <v-main>
-        <ColorPicker></ColorPicker>
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script setup>
-  import ColorPicker from "@/components/ColorPicker.vue";
-  </script>
+  <v-app>
+    <v-main>
+      <ColorPicker></ColorPicker>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import ColorPicker from "@/components/ColorPicker.vue";
+</script>
--- frontend_2/src/components/ColorPicker.vue
+++ frontend_2/src/components/ColorPicker.vue
@@ -1,17 +1,43 @@
 <template>
-  <v-card>
-    <span class="text-subtitle-2">Challenge 1</span>
-    <h1 class="text-h3">Color Transition</h1>
-    <v-color-picker></v-color-picker>
+  <v-card
+    class="mx-auto"
+    max-width="340"
+    theme="dark"
+    color="#2f3136"
+    elevation="12"
+    variant="flat"
+    title="Color Transition"
+    subtitle="Challenge 1"
+  >
+    <v-card-text class="mt-4">
+      <span class="d-block text-body-2 mb-2">Initial Color:</span>
+      <v-color-picker hide-inputs v-model="color"></v-color-picker>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn @click="logColor()" variant="text" color="red" size="large"
+        >Animate!</v-btn
+      >
+    </v-card-actions>
   </v-card>
 </template>
 <script>
 export default {
   name: "ColorPicker",
+  data() {
+    return {
+      color: { r: 255, g: 0, b: 255, a: 1 },
+    };
+  },
+  methods: {
+    logColor() {
+      console.log([this.color.r, this.color.g, this.color.b, this.color.a]);
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
-.text-subtitle-2 {
-  background-color: red;
+v-card {
+  padding: 1.2rem 3rem;
 }
 </style>
