ad7dde0fc25cc6075d45bda74fcee478ce40dc80
Filip Šipoš <filipsipo@gmail.com>
2023-04-01 13:11:03 +0200

    sass

---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="navigation">
+    <nav class="navigation__nav">
+      <p>dement</p>
+    </nav>
+  </section>
+</template>
+<script>
+export default {
+  name: "Navigation",
+};
+</script>
+<style lang="scss" scoped></style>
